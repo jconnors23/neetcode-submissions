b64baabf41729df3c909835b3e9eda8e394112fd
Josiah Connors <59714706+jconnors23@users.noreply.github.com>
2026-05-06 16:00:52 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,25 @@
+# subtract index from tar, if tar minus next index = 0 return indices
+
+
+# V1: list lookup
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             diff = target - nums[i]
+#             rest = nums[i+1:] # slice for rest of nums, ignoring i and detecting if target exists
+#             if diff in rest:
+#                 j = rest.index(diff) # find val at index 
+#                 vals = [i, j+i+1] # relative positioning | i maps index from rest back to nums arr, +1 skips curr element so that i != j
+#                 return vals
+#         return None
+
+# V2: map lookup
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        vals = dict()
+        for i in range(len(nums)):
+            vals[i] = nums[i] # val { 0: 3}
+            diff = target - vals[i]
+            for k, v in vals.items():
+                if v == diff and k != i:
+                    return [k, i]
